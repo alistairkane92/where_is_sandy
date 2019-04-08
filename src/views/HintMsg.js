e26789a0bd78e5581c9e5fd102955fbf967a9d1f
Alistair Kane <alistairkane92@hotmail.co.uk>
2019-04-08 21:47:32 +0100

    bit of refactoring and actual sandy photo

--- src/views/HintMsg.js
+++ src/views/HintMsg.js
@@ -1,61 +1,26 @@
 import React from 'react';
 
-const HintMsg = (props) => {
+const HintMsg = ({ qnum }) => {
   let style = {
     animationName: 'fade-in',
     animationDuration: '2.5s',
     animationFillMode: 'forwards'
   }
 
-  if (props.qnum === 1) {
+  const hintText = {
+    1: "When both files are in the same directory, simply type the name of the file you are looking for, e.g \"./sandy\"",
+    2: "To require a file from another directory, you first have to specify which directory the file is in, e.g \"./directory/file\"",
+    3: "If the file you are lookingfor is up a directory level, use \"../\" to go up a level, e.g \"../file\"",
+    4: "Combine what you have just learnt to first go up a directory level, and then into the directory that contains the file",
+    5: "You can chain together multiple \"../\" to go up several directory levels, e.g \"../../file\"",
+    6: "This is a tricky one! Bob is in the office, but he's not in the meeting. Sandy is in the home, but he's not in the kitchen"
+  }
+
     return (
     <div id="hint-msg" style={style}>
-    <p className='hint-p'>Hint: When both files are in the same directory, simply type the name of the file you are looking for, e.g "./sandy"</p>
+    <p className='hint-p'>Hint: {hintText[qnum]}</p>
     </div>
     )
-  }
-
-  if (props.qnum === 2) {
-    return(
-    <div id="hint-msg" style={style}>
-    <p className='hint-p'>Hint: To require a file from another directory, you first have to specify which directory the file is in, e.g "./directory/file"</p>
-    </div>
-    )
-  }
-
-  if (props.qnum === 3) {
-    return(
-    <div id="hint-msg" style={style}>
-    <p className='hint-p'>Hint: If the file you are looking for is up a directory level, use "../" to go up a level, e.g "../file"</p>
-    </div>
-    )
-  }
-
-  if (props.qnum === 4) {
-    return(
-    <div id="hint-msg" style={style}>
-    <p className='hint-p'>Hint: Combine what you have just learnt to first go up a directory level, and then into the directory that contains the file</p>
-    </div>
-    )
-  }
-
-  if (props.qnum === 5) {
-    return(
-    <div id="hint-msg" style={style}>
-    <p className='hint-p'>Hint: You can chain together multiple "../" to go up several directory levels, e.g "../../file"</p>
-    </div>
-    )
-  }
-
-  if (props.qnum === 6) {
-    return(
-    <div id="hint-msg" style={style}>
-    <p className='hint-p'>Hint: This is a tricky one! Bob is in the office, but he's not in the meeting. Sandy is in the home, but he's not in the kitchen</p>
-    </div>
-    )
-  }
-
-  return null;
 }
 
 export default HintMsg;
--- src/views/StoryMsg.js
+++ src/views/StoryMsg.js
@@ -1,35 +1,18 @@
 import React from 'react';
 
-const whichMsg = (qnum) => {
-  if (qnum === 1){
-    return "Bob is having a wonderful day at home with his best pal Sandy. Where has he got to?"
-  }
-
-  if (qnum === 2){
-    return "Bob is taking Sandy out for a walk... but Sandy seems to have ran away!"
-  }
-
-  if (qnum === 3){
-    return "Sometimes, Sandy likes to wait for Bob outside the office."
-  }
-
-  if (qnum === 4){
-    return "Bob has to get out of work before he can go home to Sandy."
-  }
-
-  if (qnum === 5){
-    return "Bob was in an important meeting when he remembered that he left Sandy at the beach. Woops!"
-  }
-
-  if (qnum === 6){
-    return "Almost there... find Sandy!"
-  }
+const whichMsg = {
+  1: "Bob is having a wonderful day at home with his best pal Sandy. Where has he got to?",
+  2: "Bob is taking Sandy out for a walk... but Sandy seems to have ran away!",
+  3: "Sometimes, Sandy likes to wait for Bob outside the office.",
+  4: "Bob has to get out of work before he can go home to Sandy.",
+  5: "Bob was in an important meeting when he remembered that he left Sandy at the beach. Woops!",
+  6: "Almost there... find Sandy!",
+  7: "You found him!"
 }
 
-
-const StoryMsg = (props) => {
+const StoryMsg = ({ qnum }) => {
   return(
-    <p id="story-msg">{whichMsg(props.qnum)}</p>
+    <p id="story-msg">{whichMsg[qnum]}</p>
   )
 }
 
